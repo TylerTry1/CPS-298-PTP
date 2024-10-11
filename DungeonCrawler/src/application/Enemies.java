80aa47ad27a5ed8767d398c27fd057fb57061034
EmbersAlice <killian.alice.p@gmail.com>
2024-10-11 01:17:44 -0400

    Enemy Stats
    Statted Out all of the current enemies we had listed in the google doc including Body Pile for Necromancer, with optional parameter for position.

--- DungeonCrawler/src/application/Enemies.java
+++ DungeonCrawler/src/application/Enemies.java
@@ -1,5 +1,230 @@
 package application;
 
+//import java.util.ArrayList;
+import java.util.Optional; // For Optional Parameters
 public class Enemies {
-
+	
+	public class enemyStats {
+        String name;
+        int health;
+        int damage;
+        int defense; // leave this stat at 0 for all characters, need a 0 value in case we want to change it later.
+        int accuracy;
+        int crit_chance;
+        int dodge_chance; // leave this stat at 0 for all characters, need a 0 value in case we want to change it later.
+        int position;
+        int size;
+	}
+	
+	public class goblinScout extends enemyStats {
+		public goblinScout(Optional <Integer> pos) {
+			name = "Goblin Scout";
+			health = 24;
+			damage = 7;
+			defense = 0;
+			accuracy = 70;
+			crit_chance = 10;
+			dodge_chance = 0;
+			position = pos.orElse(2);
+			size = 1;
+		}
+		
+	}
+	
+	public class goblinAxeman extends enemyStats {
+		public goblinAxeman(Optional <Integer> pos) {
+			name = "Goblin Axemant";
+			health = 32;
+			damage = 15;
+			defense = 0;
+			accuracy = 75;
+			crit_chance = 20;
+			dodge_chance = 0;
+			position = pos.orElse(1);
+			size = 1;
+		}
+		
+	}
+	
+	public class goblinArcher extends enemyStats {
+		public goblinArcher(Optional <Integer> pos) {
+			name = "Goblin Scout";
+			health = 28;
+			damage = 10;
+			defense = 0;
+			accuracy = 85;
+			crit_chance = 15;
+			dodge_chance = 0;
+			position = pos.orElse(3);
+			size = 1;
+		}
+		
+	}
+	
+	public class goblinShaman extends enemyStats {
+		public goblinShaman(Optional <Integer> pos) {
+			name = "Goblin Shaman";
+			health = 20;
+			damage = 4;
+			defense = 0;
+			accuracy = 65;
+			crit_chance = 10;
+			dodge_chance = 0;
+			position = pos.orElse(4);
+			size = 1;
+		}
+		
+	}
+	
+	public class skeletonSwordsman extends enemyStats {
+		public skeletonSwordsman(Optional <Integer> pos) {
+			name = "Skeleton Swordsman";
+			health = 28;
+			damage = 12;
+			defense = 0;
+			accuracy = 75;
+			crit_chance = 12;
+			dodge_chance = 0;
+			position = pos.orElse(1);
+			size = 1;
+		}
+		
+	}
+	
+	
+	public class skeletonCrossbowmen extends enemyStats {
+		public skeletonCrossbowmen(Optional <Integer> pos) {
+			name = "Skeleton Crossbowmen";
+			health = 25;
+			damage = 10;
+			defense = 0;
+			accuracy = 90;
+			crit_chance = 20;
+			dodge_chance = 0;
+			position = pos.orElse(2);
+			size = 1;
+		}
+		
+	}
+	
+	public class skeletonDefender extends enemyStats {
+		public skeletonDefender(Optional <Integer> pos) {
+			name = "Skeleton Defender";
+			health = 35;
+			damage = 15;
+			defense = 0;
+			accuracy = 85;
+			crit_chance = 15;
+			dodge_chance = 0;
+			position = pos.orElse(1);
+			size = 1;
+		}
+		
+	}
+	
+	public class necromancer extends enemyStats {
+		public necromancer(Optional <Integer> pos) {
+			name = "Necromancer";
+			health = 25;
+			damage = 12; 
+			defense = 0;
+			accuracy = 70;
+			crit_chance = 0;
+			position = pos.orElse(4);
+			size = 1;
+		}
+	}
+	
+	public class bodyPile extends enemyStats {
+		public bodyPile(Optional <Integer> pos) {
+			name = "Body Pile";
+			health = 20;
+			damage = 0;
+			defense = 0;
+			accuracy = 0;
+			crit_chance = 0;
+			position = pos.orElse(1);
+			size = 1;
+		}
+	}
+	
+	public class giantPummeler extends enemyStats {
+		public giantPummeler(Optional <Integer> pos) {
+			name = "Giant Pummeler";
+			health = 50;
+			damage = 25;
+			defense = 0;
+			accuracy = 65;
+			crit_chance = 5;
+			position = pos.orElse(1);
+			size = 2;
+		}
+	}
+	
+	public class giantSlammer extends enemyStats {
+		public giantSlammer(Optional <Integer> pos) {
+			name = "Giant Slammer";
+			health = 60;
+			damage = 30;
+			defense = 0;
+			accuracy = 70;
+			crit_chance = 8;
+			position = pos.orElse(1);
+			size = 2;
+		}
+	}
+	
+	public class zombiePeasant extends enemyStats{
+		public zombiePeasant(Optional <Integer> pos) {
+			name = "Zombie Peasant";
+			health = 18;
+			damage = 10;
+			defense = 0;
+			accuracy = 50;
+			crit_chance = 5;
+			position = pos.orElse(1);
+			size = 1;
+			
+		}
+	}
+	
+	public class zombieKnight extends enemyStats{
+		public zombieKnight(Optional <Integer> pos) {
+			name = "Zombie Knight";
+			health = 35;
+			damage = 25;
+			defense = 0;
+			accuracy = 60;
+			crit_chance =15;
+			position = pos.orElse(1);
+			size = 1;
+			
+		}
+	}
+	
+	public class minotaur extends enemyStats {
+		public minotaur(Optional <Integer> pos) {
+			name = "Minotaur";
+			health = 65;
+			damage = 30;
+			defense = 0;
+			accuracy = 80;
+			crit_chance = 25;
+			position = pos.orElse(1);
+			size = 2;
+		}
+	}
+	
+	public class boar extends enemyStats {
+		public boar(Optional <Integer> pos) {
+			name = "Boar";
+			health = 40;
+			damage = 18;
+			defense = 0;
+			accuracy = 50;
+			crit_chance = 20;
+			position = pos.orElse(1);
+			size = 1;
+		}
+	}
 }
