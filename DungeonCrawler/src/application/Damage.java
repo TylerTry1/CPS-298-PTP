936e5498e9097ca53a9d9bdf143b1864a6dc855e
Terminalboot1 <chunt6@wccnet.edu>
2024-10-14 10:49:48 -0400

    Damage starter

--- DungeonCrawler/src/application/Damage.java
+++ DungeonCrawler/src/application/Damage.java
@@ -1,5 +1,34 @@
 package application;
 
-public class Damage {
+import java.util.Random;
 
+public class Damage {
+    private Random random;
+
+    public Damage() {
+        this.random = new Random();
+    }
+
+    public int calculateDamage(characterStats attacker, characterStats defender) {
+        int baseDamage = attacker.damage;
+
+        if (isCriticalHit(attacker)) {
+            baseDamage *= 2;
+            System.out.println(attacker.name + " scored a critical hit!");
+        }
+        return baseDamage;
+    }
+
+    private boolean isCriticalHit(characterStats attacker) {
+        return random.nextInt(100) < attacker.crit_chance;
+    }
+
+    public void applyDamage(characterStats defender, int damage) {
+        defender.health -= damage;
+        if (defender.health < 0) {
+            defender.health = 0;
+        }
+        System.out.println(defender.name + " takes " + damage + " damage! Remaining health: " + defender.health);
+    }
 }
+
